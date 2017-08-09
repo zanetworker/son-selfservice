f41e490166cab72a8506914b5583cc6904d466d7
Adel Zaalouk <adel.zaalouk@neclab.eu>
2017-08-09 13:34:02 +0200

    add readme for backend

---
+++ selfservice-backend/glide.yaml
@@ -0,0 +1,6 @@
+package: github.com/zanetworker/son-selfservice/selfservice-backend
+import:
+- package: github.com/Sirupsen/logrus
+  version: ^1.0.2
+- package: github.com/gorilla/websocket
+- package: github.com/mitchellh/mapstructure
--- selfservice-backend/handler.go
+++ selfservice-backend/handler.go
@@ -9,17 +9,22 @@ import (
 //StartFSM command to send the SSM to start a specific FSM
 func StartFSM(client *Client, fsmInputData interface{}) {
 	var fsmData models.FSMAction
+	var fsmDataReply models.Message
 	if err := mapstructure.Decode(fsmInputData, &fsmData); err != nil {
 		log.Error(err)
 	}
 
 	//TODO send command to the SSM
+	fsmDataReply.Name = "fsm start"
+	fsmDataReply.Data = fsmData
+	client.send <- fsmDataReply
+
 	log.Infof("%#v\n", fsmData)
 	log.Info("started FSM")
 }
 
 //StopFSM command to send the SSM to stop a specific FSM
-func StopFSM(fsmInputData interface{}) error {
+func StopFSM(client *Client, fsmInputData interface{}) error {
 	//TODO
 	return nil
 }
